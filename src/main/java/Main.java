7415ae6cce566a40eced39b293f052bfc68578a1
notedwlove <132625901+notedwlove@users.noreply.github.com>
2023-11-25 08:07:16 -0300

    docentRepositoty updated

---
+++ src/main/java/Main.java
@@ -0,0 +1,47 @@
+import repository.StudentRepository;
+import service.*;
+import entity.*;
+
+import javax.print.Doc;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        DocentService docentService = new DocentService();
+//        Docent docent1 = new Docent();
+//        docent1.setvoornaam("Suguru");
+//        docent1.setachternaam("Geto");
+//        docent1.setDatumIndienst("23-04-2018");
+//        docent1.setOnderdeel("JJK");
+//        docentService.insertDocent(docent1);
+//        //--------------------------------------------------
+//        Docent docent2 = new Docent();
+//        docent2.setvoornaam("Satoru");
+//        docent2.setachternaam("Gojo");
+//        docent2.setDatumIndienst("23-04-2018");
+//        docent2.setOnderdeel("JJK");
+//        docentService.insertDocent(docent2);
+//        //--------------------------------------------------
+//        Docent docent3 = new Docent();
+//        docent3.setvoornaam("Kento");
+//        docent3.setachternaam("Nanami");
+//        docent3.setDatumIndienst("22-03-2017");
+//        docent3.setOnderdeel("JJK");
+//        docentService.insertDocent(docent3);
+//        //--------------------------------------------------
+//        Docent docent4 = new Docent();
+//        docent4.setvoornaam("Koro");
+//        docent4.setachternaam("-Sensei");
+//        docent4.setDatumIndienst("18-11-2015");
+//        docent4.setOnderdeel("Murder");
+//        docentService.insertDocent(docent4);
+//
+//        List<Docent> docenten = docentService.getAllDocenten();
+//        docenten.forEach(docent -> {
+//            System.out.println(" Voornaam = " + docent.getvoornaam() + " Achternaam = " +
+//                    docent.getachternaam() + " In dienst datum = " + docent.getDatumIndienst() +
+//                    " onderdeel = " + docent.getOnderdeel());
+//        });
+    }
+}
